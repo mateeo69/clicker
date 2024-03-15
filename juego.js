b3077a852e0b58bce264bbd6260aff784c706725
Ruben Benitez <rubenbeniteznavas@gmail.com>
2024-03-15 17:14:34 +0100

    refactor JS

--- juego.js
+++ juego.js
@@ -60,23 +60,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
-function changeOption (opcion){
-    if (opcion == 1) {
-        option2.classList.remove('option-active');
-        content2.classList.remove('content-active');
-        option1.classList.add('option-active');
-        content1.classList.add('content-active');
-    } else if (opcion == 2) {
-        option1.classList.remove('option-active');
-        content1.classList.remove('content-active');
-        option2.classList.add('option-active');
-        content2.classList.add('content-active');
-    }
-};
-
-
-
 // Cuando haces click en la tela
 function clickTela(){
     tela++;
